45fad8a3adc8aed068dc3fb5aab8ffc64eadf175
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-09 20:34:44 +0700

    penggunaan data dinamis pada react tool dengan menggunakan 'props.children'

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import Welcome, {Welcome2, Welcome3} from './welcome';
+import {Welcome2} from './welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Welcome/>
-        <Welcome2/>
-        <Welcome3/>
-      </header>
+        <Welcome2 children='Welcome 1'/>
+        <Welcome2 children='Welcome 2'/>
     </div>
   );
 }
--- src/welcome.js
+++ src/welcome.js
@@ -1,14 +1,9 @@
-function Welcome () {
-    return <h1>Welcome</h1>;
-
-}
-
-export function Welcome2 () {
-    return <h2>Welcome 2</h2>
+export function Welcome2 (props) {
+    console.log(props);
+    return <h2>{props.children}</h2>
 }
 
 export function Welcome3 () {
     return <h3>Welcome 3</h3>
 }
 
-export default Welcome;
