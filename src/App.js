4857482372f844456832a0dea671a5376af1069a
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-02-10 07:00:02 +0700

    akses image folder root

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import {Welcome2} from './welcome';
 
 function App() {
   return (
     <div className="App">
-        <Welcome2 children='Welcome 1'/>
-        <Welcome2 children='Welcome 2'/>
+      <img src='/logo512.png' alt=''/>
     </div>
   );
 }
--- src/welcome.js
+++ src/welcome.js
@@ -1,9 +1,4 @@
-export function Welcome2 (props) {
-    console.log(props);
-    return <h2>{props.children}</h2>
-}
-
-export function Welcome3 () {
-    return <h3>Welcome 3</h3>
+export function Button (props) {
+    return <button className="btn">{props.children}</button>;
 }
 
