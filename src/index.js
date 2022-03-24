662378ffc38e0dc73c9a4a902469e80873d79758
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-03-24 20:40:24 +0700

    update pages

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import store from '../src/redux/store';
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App/>
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </BrowserRouter>          
   </React.StrictMode>,
   document.getElementById('root')
