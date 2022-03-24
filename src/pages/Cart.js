cbc40ee773cff1489e60561f4e3538a23e9cb3a8
Achmad Dicky Rizhaldi <37206097+dikyriz@users.noreply.github.com>
2022-03-24 20:41:20 +0700

    add pages

---
+++ src/pages/Cart.js
@@ -0,0 +1,110 @@
+import { KausHitam, KausMerah } from "../assets";
+import { decrement, decrements, increment, increments } from "../redux/actions/counter";
+import {useSelector, useDispatch} from "react-redux";
+import { createRef, useEffect, useState } from "react";
+import store from '../redux/store';
+
+export default function Cart(props) {
+    const counter = useSelector((state) => state.counter);
+    console.log(counter);
+    const counters = useSelector((state) => state.counters);
+    const dispatch = useDispatch();
+    const dispatchs = useDispatch();
+    const [totals, setTotals] = useState();
+    const [price, setPrice] = useState();
+    const [payment, setPayment] = useState();
+
+    ////////////
+    let grab = payment;
+    let teks = grab.toString();
+        let result = teks.substring(0, 2);
+        let results = teks.substring(2, 4);
+        let combine_ress = result + "." + results;
+        let number = parseFloat(combine_ress);
+        console.log(number);
+
+    useEffect(function (){
+        setPrice(1799 * counter.value);
+        setTotals(3599 * counters.valuee);
+        setPayment(price + totals);
+       
+        
+        // let price = 1799 * counter.value;
+        // console.log('totals ' + price);
+    });
+    
+    return(
+    <section>
+    <h1 className="title">Shopping Cart</h1>
+    <div className="container">
+        <div className="card">
+                <h3 className="title-card">Cart (2 items)</h3>
+                <div className="card-container">
+                    <img src={KausMerah} alt='kaus-merah' className="img-kaus" />
+                <div className="card-desc">
+                    <h5 className="card-title">Red Shirt</h5>
+                    <p className="card-text">T-SHIRT01 <span className="text-child">RED</span></p>
+                    <p className="card-text">COLOR <span className="text-child">RED</span></p>
+                    <p className="card-text">SIZE <span className="text-child">M</span></p>           
+                    <div className="icons">
+                        <i className="fa-solid fa-trash icon"><span className="icon-desc">REMOVE ITEM</span></i>
+                        <i className="fa-solid fa-heart icon"><span className="icon-desc">MOVE TO WISH LIST</span></i>
+                    </div>
+                </div>
+                <div className="input">
+                    <div className="input-container">
+                        <button onClick={() => dispatch(decrement())} className="btn btn-cart"><i className="fa-solid fa-minus"></i></button>
+                        <span className="input-child">{counter.value}</span>     
+                        <button onClick={() => dispatch(increment())} className="btn btn-cart"><i className="fa-solid fa-plus"></i></button>
+                        <p className="input-desc">(Note, 1 place)</p>
+                    </div>
+                    <div className="pay">
+                        <p className="payment">$17.99</p>
+                    </div>
+                </div>
+                </div>
+                <div className="borders"></div>
+                <div className="card-container">
+                    <img src={KausHitam} alt='kaus-hitam' className="img-kaus" />
+                <div className="card-desc">
+                    <h5 className="card-title">BLACK Shirt</h5>
+                    <p className="card-text">T-SHIRT02 <span className="text-child">BLACK</span></p>
+                    <p className="card-text">COLOR <span className="text-child">BLACK</span></p>
+                    <p className="card-text">SIZE <span className="text-child">M</span></p>           
+                    <div className="icons">
+                        <i className="fa-solid fa-trash icon"><span className="icon-desc">REMOVE ITEM</span></i>
+                        <i className="fa-solid fa-heart icon"><span className="icon-desc">MOVE TO WISH LIST</span></i>
+                    </div>
+                </div>
+                <div className="input">
+                    <div className="input-container">
+                        <button onClick={() => dispatchs(decrements())} className="btn btn-cart"><i className="fa-solid fa-minus"></i></button>
+                        <span className="input-child">{counters.valuee}</span>                
+                        <button onClick={() => dispatchs(increments())} className="btn btn-cart"><i className="fa-solid fa-plus"></i></button>
+                    </div>
+                    <div className="pay">
+                        <p className="payment">$35.99</p>
+                    </div>
+                </div>
+                </div>
+        </div>
+        <div className="totals">
+            <div className="card">
+                <h3 className="title-card">The total amount of</h3>
+                <div className="container-totals">
+                    <p className="payments">Temporary Amount <span className="payments-ship-child">${number}</span></p>
+                    <p className="shipping-cart">Shipping <span className="payments-ship-child">Gratis</span></p>
+                    <div className="borders-totals"></div>
+                    <h4 className="totals-payment">The total amount of <span className="totals-payment-child">(Including VAT)<span className="payments-child">${number}</span></span></h4>
+                </div>
+                <button className="btn btn-primary">GO TO CHECKOUT</button>
+            </div>
+            <div className="card">
+                <p className="optional">Add a discount code [optional] <span className="icon-payment"><i className="fa-solid fa-angle-down"></i></span></p>
+            </div>
+        </div>
+        
+    </div>
+    </section>
+    );
+}
---
+++ src/pages/data.js
@@ -0,0 +1,20 @@
+const data = [
+        {
+            id: '1',
+            name: 'Red Shirt',
+            type: 'T-SHIRT01',
+            color: 'RED',
+            size: 'M',
+            price: 1799,
+        },
+        {
+            id: '2',
+            name: 'BLACK Shirt',
+            type: 'T-SHIRT02',
+            color: 'BLACK',
+            size: 'M',
+            price: 3599,
+        }
+    ];
+export default data;
+
